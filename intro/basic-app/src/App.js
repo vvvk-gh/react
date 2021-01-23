51f75d3243b8f05dd185c41cfa6797f9f0d73ba8
vvvk-gh <vamsivutukuri1997@gmail.com>
2021-01-24 00:31:50 +0530

    Custom Component

--- intro/basic-app/src/App.js
+++ intro/basic-app/src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './Greet'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Hope this changes will reflect in live
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet />   {/* Displays the Greet Component */}
     </div>
   );
 }
