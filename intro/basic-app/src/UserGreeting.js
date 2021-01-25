2ee4633920dbf96c42ff10dd7003b103e0ef134b
vvvk-gh <vamsivutukuri1997@gmail.com>
2021-01-25 12:15:31 +0530

    element variables

--- intro/basic-app/src/UserGreeting.js
+++ intro/basic-app/src/UserGreeting.js
@@ -5,27 +5,39 @@ class UserGreeting extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoggedIn : true 
+            isLoggedIn : true
         }
     }
     render() {
-        // if else conditional
-        if(this.state.isLoggedIn){
-            return (
-                <div>
-                    <p>Hello Vishwas</p>
-                </div>
-            )
-        }
-        else{
-            return (
-                <div>
-                    <p>Hello Guest</p>
-                </div>
-            )
-        }
-        
+
+ //Element variables 
+    let message;
+    if(this.state.isLoggedIn){
+        message = <div>Hello Vishwas</div>
     }
+    else{
+        message = <div>Hello Guest</div>
+    }
+    return <div>{message}</div>;
+        
+
+// if else conditional
+    //     if(this.state.isLoggedIn){
+    //         return (
+    //             <div>
+    //                 <p>Hello Vishwas</p>
+    //             </div>
+    //         )
+    //     }
+    //     else{
+    //         return (
+    //             <div>
+    //                 <p>Hello Guest</p>
+    //             </div>
+    //         )
+    //     }
+
+     }
 }
     
 export default UserGreeting;
