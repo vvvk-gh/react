df452e8b20af3ffdffd94ffa31f8f45dc9c5f263
vvvk-gh <vamsivutukuri1997@gmail.com>
2021-01-26 13:06:13 +0530

    react controllable select

--- intro/basic-app/src/Form.js
+++ intro/basic-app/src/Form.js
@@ -4,25 +4,43 @@ class Form extends Component {
     constructor(props) {
         super(props)
         this.state = {
-             value : '',
+             username : '',
+             optionValue : '',
         }
     }
     
     changeHandler = (event) =>{
         this.setState({
-            value : event.target.value,
+            username : event.target.value,
         })
     }
 
+    selectHandler = event =>{
+        this.setState({
+            optionValue : event.target.value,
+        })
+    }
     render(){
         return (
             <form>
                 <h1>Form</h1>
-                <label htmlFor="username">Name </label>
                 {/* normal field */}
                 {/* <input type="text"/> */}
+
+                <div>
+                <label htmlFor="username">Name </label>
                 {/* controlled fields */}
-                <input id="username" type="text" value = {this.state.value} onChange ={this.changeHandler}/>
+                <input id="username" type="text" value = {this.state.username} onChange ={this.changeHandler}/>
+                </div>
+
+                <div>
+                    <select value = {this.state.value} onChange={this.selectHandler}>
+                        <option value="react">React</option>
+                        <option value="angular">Angular</option>
+                        <option value="vue">Vue</option>
+                    </select>
+                </div>
+               
             </form>
         )
     }
