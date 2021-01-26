3206f0874440b5257b28c3f2cf8502faf2a280f2
vvvk-gh <vamsivutukuri1997@gmail.com>
2021-01-26 13:28:04 +0530

    controllable components

--- intro/basic-app/src/Form.js
+++ intro/basic-app/src/Form.js
@@ -9,6 +9,7 @@ class Form extends Component {
         }
     }
     
+
     changeHandler = (event) =>{
         this.setState({
             username : event.target.value,
@@ -20,9 +21,15 @@ class Form extends Component {
             optionValue : event.target.value,
         })
     }
+
+    sumbitHandler = event => {
+        alert(`${this.state.username} ${this.state.optionValue}`)
+    }
     render(){
+        const { username , optionValue} = this.state;
+
         return (
-            <form>
+            <form onSubmit={this.sumbitHandler}>
                 <h1>Form</h1>
                 {/* normal field */}
                 {/* <input type="text"/> */}
@@ -30,17 +37,18 @@ class Form extends Component {
                 <div>
                 <label htmlFor="username">Name </label>
                 {/* controlled fields */}
-                <input id="username" type="text" value = {this.state.username} onChange ={this.changeHandler}/>
+                <input id="username" type="text" value = {username} onChange ={this.changeHandler}/>
                 </div>
 
                 <div>
-                    <select value = {this.state.value} onChange={this.selectHandler}>
+                    <select value = {optionValue} onChange={this.selectHandler}>
                         <option value="react">React</option>
                         <option value="angular">Angular</option>
                         <option value="vue">Vue</option>
                     </select>
                 </div>
-               
+
+                <input type="submit" value="submit"/>
             </form>
         )
     }
