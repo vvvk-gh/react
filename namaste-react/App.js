9b232e2893895a0d818bf8af0fd96fb47bfb6adf
vvvk-gh <vamsivutukuri1997@gmail.com>
2023-12-25 19:41:12 +0530

    nested sibling react element

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,15 +1,21 @@
 {/* 
     CREATING A NESTED REACT ELEMENT
 <div id="parent">
-<div id="child">
-<h1>Im a H1 tag</h1>
-</div>
+    <div id="child">
+        <h1>Im a h1 tag</h1>
+        <h2>Im a h2 tag</h2>
+    </div>
 </div> */}
 
 
-const h1Family = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" }, React.createElement('h1', {
-    id: 'heading'
-}, 'Im the H1 tag')));
+const h1Family = React.createElement('div', { id: "parent" },
+    React.createElement('div', { id: "child" },
+        [
+            React.createElement('h1', { id: 'heading' }, 'Im h1 tag'),
+            React.createElement('h2', { id: 'heading2' }, 'Im h2 tag ')
+        ]
+    )
+);
 
 console.log(h1Family); //returns object and not html tag
 
