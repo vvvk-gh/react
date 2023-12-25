7afee85346bf37bc303fefed2fe8ee7831699e9a
vvvk-gh <vamsivutukuri1997@gmail.com>
2023-12-25 19:22:22 +0530

    nested rect element

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,9 +1,17 @@
-const heading = React.createElement(
-    'h1',
-    {
-        id: "heading"
-    },
-    'Hello World from React'
-);
+{/* 
+    CREATING A NESTED REACT ELEMENT
+<div id="parent">
+<div id="child">
+<h1>Im a H1 tag</h1>
+</div>
+</div> */}
+
+
+const h1Family = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" }, React.createElement('h1', {
+    id: 'heading'
+}, 'Im the H1 tag')));
+
+console.log(h1Family); //returns object and not html tag
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(h1Family);
