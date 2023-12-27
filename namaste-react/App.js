9e30962de73ebe794ac8e03b8cf160cfb33f8789
vvvk-gh <vamsivutukuri1997@gmail.com>
2023-12-27 13:50:31 +0530

    Homepage Food ordering app-basic structure

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,23 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-//React Component
-//Always Start with a capital Letter
+//Building a app like Swiggy
 
-//React Functional component : A function that returns a JSX 
+/** 
+ * Homepage
+    - Logo
+    - Nav-items
 
-//component composition
-const Title = () => (<h1>This is a Title of the chapter</h1>);
+ * Body
+    - Search
+    - Restuarent container
+        -Restuarent card
+            - Img
+            - Cusines
+            - ratings
+            - delivery time
 
-const HeadingComponent = () => (
-    <div className="container">
-        <Title />
-        <h2>This is sub heading component</h2>
-    </div>
+* Footer
+    - Copyright
+    - Links
+    - Address
+    - Contact
+*/
 
-);
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img src="https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg" alt="site logo" className="logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Homepage</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
+const RestuarentCard = () => {
+    return (
+        <div className='res-card'>
+            <img src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/pouring-honey-on-pancakes.jpg"
+                alt="restaurent logo"
+                className='res-logo'
+            />
+            <div className="res-details">
+                <div className="res-name">
+                    <h3>Pancakes 99</h3>
+                </div>
+                <h4>Breakfast, Tiffins, Desserts</h4>
+                <h4>4.4 stars</h4>
+                <h5>38 mins</h5>
+            </div>
+        </div>)
+}
+
+const Body = () => {
+    return (<div className="body">
+        <div className="res-container">
+            <RestuarentCard />
+            <RestuarentCard />
+            <RestuarentCard />
+            <RestuarentCard />
+            <RestuarentCard />
+            <RestuarentCard />
+            <RestuarentCard />
+            <RestuarentCard />
+            <RestuarentCard />
+            <RestuarentCard />
+        </div >
+    </div>)
+}
+
+const AppLayout = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<HeadingComponent />);
+root.render(<AppLayout />)
