1097e87299c997925fe637aa559c4b95a818a14a
vvvk-gh <vamsivutukuri1997@gmail.com>
2024-01-02 00:12:37 +0530

    multiple state variables

--- namaste-react/src/components/UserClass.js
+++ namaste-react/src/components/UserClass.js
@@ -3,12 +3,21 @@ import React from 'react';
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            count: 0,
+            count2: 2,
+        };
     }
     render() {
+        const { name, job } = this.props;
+        const { count, count2 } = this.state;
         return (
             <div className="about-card">
-                <h3>{this.props.name}</h3>
-                <h4>{this.props.job}</h4>
+                <h5>Count : {count}</h5>
+                <h5>Count2 : {count2}</h5>
+                <h3>{name}</h3>
+                <h4>{job}</h4>
                 <h5>@vvvk-gh</h5>
             </div>
         )
